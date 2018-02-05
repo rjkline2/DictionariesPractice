2fb009cd6cc34d5240e5939b2b75441f53458c96
RJ Kline <rj.kline@gmail.com>
2018-02-05 10:16:20 -0500

    Project Update

--- DictionariesPractice/Program.cs
+++ DictionariesPractice/Program.cs
@@ -18,7 +18,29 @@ namespace DictionariesPractice
                 {"Michelle", 100 }
             };
 
-       
+            Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            {
+                {"A1", "Snickers" },
+                {"A2" , "Swedish Fish"},
+                {"A3", "Sourpatch Kids" },
+                {"A4", "Funyuns" }
+            };
+
+            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            // Empty dictionary
+            //Using add method, let's add drinks to our drink machine...
+            //a letter-number pair
+
+            drinkMachine.Add(10, "Ginger Ale");
+            drinkMachine.Add(11, "Coke");
+            drinkMachine.Add(12, "Pepsi");
+            drinkMachine.Add(13, "Sunkist");
+            drinkMachine.Add(14, "Sierra Mist");
+            drinkMachine.Add(15, "LeCroix");
+
+            //count is a property.
+            Console.WriteLine(drinkMachine.Count);
+
         }
     }
 }
