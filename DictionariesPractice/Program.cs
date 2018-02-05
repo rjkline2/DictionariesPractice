8191761ab5b65f2a6a005924e58b9a58c8f0bed9
RJ Kline <rj.kline@gmail.com>
2018-02-05 09:59:27 -0500

    first commit

---
+++ DictionariesPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Syntax - Dictionary<key data type, value data type> dictionaryName
+            Dictionary<string, int> studentGrades = new Dictionary<string, int>()
+            {
+                {"Kerry", 90 },
+                {"Barry", 85 },
+                {"Michelle", 100 }
+            };
+
+        }
+    }
+}
